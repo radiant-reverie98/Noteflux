eab09fb3b2809aa68a938c0165b61fdac3216b0a
Tejendra Singh <tejendrambmcse00098@gmail.com>
2025-07-14 18:17:39 +0530

    Added gitignore

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+require('dotenv').config();
+const port = process.env.PORT
 
 app.get('/',(req,res)=>{
     res.send("Server running successfully on port 3000...")
