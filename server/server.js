a16a9cc3818d03a7da1c917a20e11698c18ef3b0
Tejendra Singh <tejendrambmcse00098@gmail.com>
2025-07-17 10:55:40 +0530

    Changed origin

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Middlewares
 app.use(cors({
-  origin: ["http://localhost:5173", "https://noteflux-mu.vercel.app"],
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
